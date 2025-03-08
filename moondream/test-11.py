dec596818f828b9f84d3a6c7117f64faf2d46064
Samuel Alaniz <samuelalaniz@wireless-10-145-41-134.public.utexas.edu>
2025-03-08 16:12:55 -0600

    test 11 is cpu and is maxed out

--- moondream/test-11.py
+++ moondream/test-11.py
@@ -6,16 +6,22 @@ import time
 # up-front to avoid decompression overhead every time the model is initialized.
 model = md.vl(model="/Users/samuelalaniz/dev/moondream-0_5b-int4.mf")
 
+
+
 # Load and process image
 image = Image.open("/Users/samuelalaniz/dev/burger-1.png")
+cpu_optimal_size = (224, 224)
+resized_image = image.resize(cpu_optimal_size, Image.BILINEAR)
+rgb_image = resized_image.convert('RGB')
+
 start_time = time.time()
-encoded_image = model.encode_image(image)
+encoded_image = model.encode_image(resized_image)
 delta_time = time.time() - start_time
 print(f"Image encoding time: {delta_time:.4f} seconds")
 
 # Generate caption
 start_time = time.time()
-answer = model.query(encoded_image, "Is the human eating food? respond with yes or no.")["answer"]
+answer = model.query(encoded_image, "Human eating food? (yes/no)")["answer"]
 delta_time = time.time() - start_time
 print(f"Image query time: {delta_time:.4f} seconds")
 
