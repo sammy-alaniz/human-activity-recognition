d80a57e0c13bcdd3caf9f93f046652db714e19d6
Samuel Alaniz <samuelalaniz@wireless-10-145-42-182.public.utexas.edu>
2025-04-04 15:27:02 -0500

    smolllm with mlx running

---
+++ smol-vlm/mlx-smol.py
@@ -0,0 +1,22 @@
+import mlx.core as mx
+from mlx_vlm import load, generate
+from mlx_vlm.prompt_utils import apply_chat_template
+from mlx_vlm.utils import load_config
+
+# Load the model
+model_path = "/Users/samuelalaniz/.cache/huggingface/hub/models--mlx-community--SmolVLM-Instruct-bf16/snapshots/cae61cdedd0602419b43b6102dc33cd9f1e929a6"
+model, processor = load(model_path)
+config = load_config(model_path)
+
+# Prepare input
+image = ["http://images.cocodataset.org/val2017/000000039769.jpg"]
+prompt = "Describe this image."
+
+# Apply chat template
+formatted_prompt = apply_chat_template(
+    processor, config, prompt, num_images=len(image)
+)
+
+# Generate output
+output = generate(model, processor, formatted_prompt, image, verbose=True)
+print(output)
--- smol-vlm/smol.py
+++ smol-vlm/smol.py
@@ -3,6 +3,10 @@ from transformers.image_utils import load_image
 
 from transformers import AutoProcessor, AutoModelForVision2Seq
 import torch
+
+import time
+
+
 DEVICE = "mps" if torch.mps.is_available() else "cpu"
 
 print(f'Device : {DEVICE}')
@@ -11,8 +15,6 @@ processor = AutoProcessor.from_pretrained("HuggingFaceTB/SmolVLM-Instruct")
 model = AutoModelForVision2Seq.from_pretrained("HuggingFaceTB/SmolVLM-Instruct",
                                                 torch_dtype=torch.bfloat16,
                                                 _attn_implementation="flash_attention_2" if DEVICE == "cuda" else "eager").to(DEVICE)
-
-
 
 # Load images
 image1 = load_image("https://huggingface.co/spaces/HuggingFaceTB/SmolVLM/resolve/main/example_images/rococo.jpg")
@@ -35,11 +37,15 @@ prompt = processor.apply_chat_template(messages, add_generation_prompt=True)
 inputs = processor(text=prompt, images=[image1, image2], return_tensors="pt")
 inputs = inputs.to(DEVICE)
 
+print('About to generate tokens')
+start_time = time.time()
 # Generate outputs
 generated_ids = model.generate(**inputs, max_new_tokens=500)
 generated_texts = processor.batch_decode(
     generated_ids,
     skip_special_tokens=True,
 )
+delta_time = time.time() - start_time
+print(f"Image generation time: {delta_time:.4f} seconds")
 
 print(generated_texts[0])
